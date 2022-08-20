016e67b247b7d29ca28eed78eca499423f961a43
username <username@cognizant.com>
2022-08-20 23:25:20 +0530

    Add project files.

---
+++ practice 12/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practice_12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nullStr = null;
+            Console.WriteLine(String.IsNullOrEmpty(nullStr));
+
+            string emptyStr2 = String.Empty;
+            Console.WriteLine(String.IsNullOrEmpty(emptyStr2));
+            Console.ReadLine();
+        }
+    }
+}
